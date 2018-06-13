265cc24a8c3198ca470f817053765844115a6c27
Aki <makitosansan@gmail.com>
2018-06-13 18:01:47 +0900

    Remove vue-script2

--- demo/app.js
+++ demo/app.js
@@ -7,41 +7,6 @@ export default {
     return {
       title: 'Vue micro:bit BLE master',
       subtitle: 'Vue 2.x component for micro:bit interface',
-      imageSelected: {
-        id: '',
-        src: '',
-        alt: ''
-      },
-      imageMultipleSelected: [],
-      dataImages: [{
-        id: '1',
-        src: 'http://placekitten.com/200/200',
-        alt: 'jQuery'
-      }, {
-        id: '2',
-        src: 'http://placekitten.com/200/200',
-        alt: 'Angular'
-      }, {
-        id: '3',
-        src: 'http://placekitten.com/200/200',
-        alt: 'Vue.js'
-      }, {
-        id: '4',
-        src: 'http://placekitten.com/200/200',
-        alt: 'React'
-      }],
-      initialSelected: [
-        {
-          id: '2',
-          src: 'http://placekitten.com/200/200',
-          alt: 'Angular'
-        },
-        {
-          id: '3',
-          src: 'http://placekitten.com/200/200',
-          alt: 'Vue.js'
-        }
-      ],
       template: ``,
     }
   },
